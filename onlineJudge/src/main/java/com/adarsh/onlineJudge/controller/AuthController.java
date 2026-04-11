2f0cb7b6f18b4d6941d33d727a99bd020db3b4d0
Adarsh Keshari <adarshps1k@gmail.com>
2026-04-11 19:33:39 +0530

    end point tested in postman with jwt

---
+++ onlineJudge/src/main/java/com/adarsh/onlineJudge/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.adarsh.onlineJudge.controller;
+
+import com.adarsh.onlineJudge.DTO.UserRegisterDTO;
+import com.adarsh.onlineJudge.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserRegisterDTO request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        String token = jwtService.generateToken(request.getEmail());
+        return ResponseEntity.ok(token);
+    }
+}
